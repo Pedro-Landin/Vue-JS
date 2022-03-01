5239c2a9df48afd083d2c2257003de801bfc8fd6
Pedro landim <henriquep35036@gmail.com>
2022-02-28 23:21:20 -0300

    Condições

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const app = Vue.createApp({
     data(){
         return{
+            showBooks: true,
             title: 'Titulo final',
             author: 'Brandon Sanderson',
             age: 45
         }
     },
         methods: {
-            changeTitle(abc){
-                this.title = abc 
-            }
-        }
-    
+          toggleShowBooks(){
+              this.showBooks = !this.showBooks
+          }
+        }  
 })
 
 app.mount('#app')
