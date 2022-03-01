873a857836451dd08e83dca2573ec4ec6420c6c4
Pedro landim <henriquep35036@gmail.com>
2022-02-28 23:01:33 -0300

    Vue JS Basics - Click Event

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
     data(){
         return{
-            title: 'Titulo final'
+            title: 'Titulo final',
+            author: 'Brandon Sanderson',
+            age: 45
         }
-    }
+    },
+        methods: {
+            changeTitle(abc){
+                this.title = abc 
+            }
+        }
+    
 })
 
 app.mount('#app')
