7fe8257f9ec4b07b4bf5eb391eceace2cfc57975
Pedro landim <henriquep35036@gmail.com>
2022-03-02 10:44:57 -0300

    Vue JS - Classes Dinamicas

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'http://www.google.com.br',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
-                { title: 'the way of kings', author: 'bradon sanderson', img: 'assets/2.jpg'},
-                { title: 'the final empire', author: 'patrick sanderson', img: 'assets/3.jpg'},
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                { title: 'the way of kings', author: 'bradon sanderson', img: 'assets/2.jpg', isFav: false},
+                { title: 'the final empire', author: 'patrick sanderson', img: 'assets/3.jpg', isFav: true},
             ]
         }
     },
