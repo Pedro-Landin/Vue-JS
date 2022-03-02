87c7a738d4afe77ffa05d5b34892647d73a7171f
Pedro landim <henriquep35036@gmail.com>
2022-03-02 10:10:13 -0300

    Vue JS - Vinculação a Atributo

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data(){
         return{
+            url: 'http://www.google.com.br',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss'},
-                { title: 'the way of kings', author: 'bradon sanderson'},
-                { title: 'the final empire', author: 'patrick sanderson'},
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
+                { title: 'the way of kings', author: 'bradon sanderson', img: 'assets/2.jpg'},
+                { title: 'the final empire', author: 'patrick sanderson', img: 'assets/3.jpg'},
             ]
         }
     },
