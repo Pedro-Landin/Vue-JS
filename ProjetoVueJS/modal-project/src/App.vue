b8fc0fe398323aeb016be3a464b201130e57eb87
Pedro landim <henriquep35036@gmail.com>
2022-03-07 11:56:45 -0300

    Emiting Customer Event

--- ProjetoVueJS/modal-project/src/App.vue
+++ ProjetoVueJS/modal-project/src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick"> Click me </button>
-<Modal :header="header" :text="text" theme="sale"/>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+  <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: { Modal },
- data(){
-  return{
-    title: 'My First Vue App :)',
-    header: 'Sign up for the Giveaway',
-    text: 'Grab your ninja swag for half price!'
-  }
- },
- methods: {
-   handleClick(){
-     console.log(this.$refs.name)
-     this.$refs.name.classList.add('active')
-     this.$refs.name.focus()
-   }
- }
-}
-
+  data() {
+    return {
+      title: "My First Vue App :)",
+      header: "Sign up for the Giveaway",
+      text: "Grab your ninja swag for half price!",
+      showModal: false
+    };
+  },
+  methods: {
+     toggleModal(){
+       this.showModal =! this.showModal
+    },
+  },
+};
 </script>
 
 <style>
