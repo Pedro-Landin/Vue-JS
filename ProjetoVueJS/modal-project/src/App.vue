d09dba3d9b602287021a74c6a1c4c34620024d0d
Pedro landim <henriquep35036@gmail.com>
2022-03-11 11:51:10 -0300

    slots

--- ProjetoVueJS/modal-project/src/App.vue
+++ ProjetoVueJS/modal-project/src/App.vue
@@ -2,7 +2,13 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-  <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  <Modal theme="sale" @close="toggleModal" >
+    <template v-slot:links>
+       <a href="#">sign up now</a>
+       <a href="#">more info</a>
+    </template>
+
+  </Modal>
   </div>
   <button @click.right="toggleModal">open modal</button>
 </template>
