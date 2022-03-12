39d913ca28c5524eb41a06c3fd927b97141e4918
Pedro landim <henriquep35036@gmail.com>
2022-03-12 18:32:51 -0300

    Estilos de metodos

--- ProjetoVueJS/reaction-timer/src/App.vue
+++ ProjetoVueJS/reaction-timer/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>Ninja Reaction Timer </h1>
-  <button @click="start">play</button>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from './components/block-main.vue'
+
 export default {
   name: 'App',
-  components: { },
+  components: { Block },
   data(){
     return {
       isPlaying: false,
@@ -17,6 +20,7 @@ export default {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      console.log(this.delay)
     }
   }
 }
