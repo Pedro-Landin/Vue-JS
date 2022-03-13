824f010dc4680e6abd118db52ae049d6076a0a30
Pedro landim <henriquep35036@gmail.com>
2022-03-12 21:49:14 -0300

    Resultado timer

--- ProjetoVueJS/reaction-timer/src/App.vue
+++ ProjetoVueJS/reaction-timer/src/App.vue
@@ -2,20 +2,22 @@
   <h1>Ninja Reaction Timer </h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/block-main.vue'
+import Results from './components/results-main.vue'
+
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results  },
   data(){
     return {
       isPlaying: false,
       delay: null,
-      socre: null,
+      score: null,
       showResults: false
     }
   },
@@ -42,5 +44,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
