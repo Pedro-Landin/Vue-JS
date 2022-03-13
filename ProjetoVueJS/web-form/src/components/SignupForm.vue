334f41bced88cc3fd9af8b9de95605f7a555a111
Pedro landim <henriquep35036@gmail.com>
2022-03-13 20:45:13 -0300

    CheckBox

--- ProjetoVueJS/web-form/src/components/SignupForm.vue
+++ ProjetoVueJS/web-form/src/components/SignupForm.vue
@@ -4,18 +4,48 @@
       <input type="email" required v-model="email">
 
       <label>Password:</label>
-      <input type="passwaord" required v-model="password">
+      <input type="password" required v-model="password">
+
+      <label>Role:</label>
+      <select v-model="role">
+          <option value="developer">Web Developer</option>
+          <option value="designer">Web Designer</option>
+      </select>
+
+      <div class="temrs">
+        <input type="checkbox" v-model="terms" required>
+        <label>Accept terms and conditions</label>
+      </div>
+
+      <div>
+          <input type="checkbox" value="shaun" v-model="names">
+          <label>Shaun</label> 
+      </div>
+      <div>
+          <input type="checkbox" value="yoshi" v-model="names">
+          <label>Yoshi</label> 
+      </div>
+      <div>
+          <input type="checkbox" value="mario" v-model="names">
+          <label>Mario</label> 
+      </div>
   </form>
 <p>Email: {{ email }}</p>
 <p>Password: {{ password }}</p>
+<p>Role: {{ role }}</p>
+<p>Terms accepted: {{ terms }}</p>
+<p>Names: {{ names }}</p>
 </template>
 
 <script>
 export default {
  data() {
      return{
-         email: '',
-         password: ''
+         email: 'mario',
+         password: '',
+         role: 'designer',
+         terms: false,
+         names: []
      }
  }
 }
@@ -39,7 +69,7 @@ label{
     letter-spacing: 1px;
     font-weight: bold;
 }
-input{
+input, select{
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -47,5 +77,12 @@ input{
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
 }
 </style>
