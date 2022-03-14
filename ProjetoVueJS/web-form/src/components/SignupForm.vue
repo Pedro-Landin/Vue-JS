4d2da643491ba6ee02a8cacf0afd77b1fb5433d0
Pedro landim <henriquep35036@gmail.com>
2022-03-13 21:52:38 -0300

    delete skill

--- ProjetoVueJS/web-form/src/components/SignupForm.vue
+++ ProjetoVueJS/web-form/src/components/SignupForm.vue
@@ -15,7 +15,7 @@
       <label>Skils:</label>
       <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
       <div v-for="skill in skills" :key="skill" class="pill">
-        {{ skill }}
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
       </div>
 
       <div class="temrs">
@@ -51,6 +51,12 @@ export default {
            }
            this.tempSkill = ''
        }
+   },
+   deleteSkill(skill) {
+       console.log(skill)
+       this.skills = this.skills.filter((item) => {
+           return skill !== item
+       })
    }
  }
 }
@@ -89,5 +95,17 @@ input[type="checkbox"]{
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+}
+.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
 }
 </style>
