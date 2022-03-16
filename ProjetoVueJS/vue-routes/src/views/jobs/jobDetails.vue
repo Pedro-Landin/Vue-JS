e60738a7124f426ba683dcd72e53abdde34268c8
Pedro landim <henriquep35036@gmail.com>
2022-03-16 12:37:05 -0300

    Route params

---
+++ ProjetoVueJS/vue-routes/src/views/jobs/jobDetails.vue
@@ -0,0 +1,17 @@
+<template>
+   <h1>The job id is {{ id }}</h1>
+</template>
+
+<script>
+export default {
+   data() {
+       return{
+           id: this.$route.params.id
+       }
+   }
+}
+</script>
+
+<style>
+
+</style>
