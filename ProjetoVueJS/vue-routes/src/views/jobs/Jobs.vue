24799602f5ebe1b9008bde1bb79fa66b4f6af52e
Pedro landim <henriquep35036@gmail.com>
2022-03-21 11:04:32 -0300

    conditional fetching data

--- ProjetoVueJS/vue-routes/src/views/jobs/Jobs.vue
+++ ProjetoVueJS/vue-routes/src/views/jobs/Jobs.vue
@@ -1,30 +1,37 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-  <router-link :to="{ name: 'jobDetails', params: { id: job.id, name: job.title}}">
-    <h2>{{ job.title }}</h2>
-  </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link
+        :to="{ name: 'jobDetails', params: { id: job.id, name: job.title } }"
+      >
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+     <p>Loading jobs...</p>
   </div>
 </template>
 
 <script>
 export default {
-   data(){
-       return {
-           jobs: []      
-      }
-   },
-   mounted() {
-     fetch('http://localhost:3000/jobs')
-       .then(res => res.json())
-       .then(data => this.jobs = data)
-       .catch(err => console.log(err.message))
-   }
-}
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
-.job h2{
+.job h2 {
   background: #f4f4f4;
   padding: 20px;
   border-radius: 10px;
@@ -33,10 +40,10 @@ export default {
   cursor: pointer;
   color: #444;
 }
-.job h2:hover{
+.job h2:hover {
   background: #ddd;
 }
-.job a{
+.job a {
   text-decoration: none;
 }
 </style>
--- ProjetoVueJS/vue-routes/src/views/jobs/jobDetails.vue
+++ ProjetoVueJS/vue-routes/src/views/jobs/jobDetails.vue
@@ -1,14 +1,29 @@
 <template>
-   <h1>The job id is {{ id }}</h1>
-   <h1>The name job is {{ name }}</h1>
+  <div v-if="job">
+    <h1>The job id is {{ id }}</h1>
+    <h1>{{ job.details }}</h1>
+    <h1>The name job is {{ job.title }}</h1>
+  </div>
+  <div v-else>
+    <P>Loading job details...</P>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['id', 'name']
-}
+  props: ["id", "name"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
